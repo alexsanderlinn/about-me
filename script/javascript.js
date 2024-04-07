7c060618adcd9d59da70e0eb4d4b94e5c9067540
alexsander <alexsanderlinn2@gmail.com>
2024-04-07 10:22:50 -0400

    update footer

--- script/javascript.js
+++ script/javascript.js
@@ -1,4 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
+const footer = document.querySelector(".footer")
 
-hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+document.getElementById('toggleNav').addEventListener('click', function() {
+    document.querySelector('.nav').classList.toggle('active');
+    document.querySelector('.footer').classList.toggle('hidden');
+});
