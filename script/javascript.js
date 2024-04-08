4367e565880bd3db8298a82d4444d7c060a9d6cd
alexsander <alexsanderlinn2@gmail.com>
2024-04-07 21:25:57 -0400

    upload

--- script/javascript.js
+++ script/javascript.js
@@ -4,5 +4,16 @@ const footer = document.querySelector(".footer")
 
 document.getElementById('toggleNav').addEventListener('click', function() {
     document.querySelector('.nav').classList.toggle('active');
-    document.querySelector('.footer').classList.toggle('hidden');
+    document.querySelector('footer').classList.toggle('hidden');
 });
+
+window.addEventListener('scroll', function() {
+    var footer = document.getElementById('footer');
+    var distanceToTop = document.body.getBoundingClientRect().bottom;
+
+    if (distanceToTop < window.innerHeight) {
+      footer.style.bottom = '0';
+    } else {
+      footer.style.bottom = '-100px';
+    }
+  });
